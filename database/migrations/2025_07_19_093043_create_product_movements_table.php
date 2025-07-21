401472c71841db406422ab72078f2d7cbe7d9d84
Rafaello <110428629+KiriGal@users.noreply.github.com>
2025-07-21 17:38:32 +0300

    Update Migrations

--- database/migrations/2025_07_19_093043_create_product_movements_table.php
+++ database/migrations/2025_07_19_093043_create_product_movements_table.php
@@ -13,12 +13,16 @@ return new class extends Migration
     {
         Schema::create('product_movements', function (Blueprint $table) {
             $table->id();
-            $table->foreign('product_id')->references('id')->on('products');
-            $table->foreign('warehouse_id')->references('id')->on('warehouses');
-            $table->foreign('order_id')->references('id')->on('orders');
+
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('warehouse_id');
+            $table->unsignedBigInteger('order_id')->nullable();
             $table->integer('delta');
             $table->string('reason')->nullable();
             $table->timestamp('created_at')->useCurrent();
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('warehouse_id')->references('id')->on('warehouses');
+            $table->foreign('order_id')->references('id')->on('orders');
         });
     }
 
